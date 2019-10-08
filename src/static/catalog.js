102172c6e2f742bab23ce410331a6f768d5698ca
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2019-10-08 12:54:51 +0700

    :truck: Add some examples

---
+++ src/static/catalog.js
@@ -0,0 +1,19 @@
+const catalog = [
+  {
+    "name": "Tiger Tank",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Leopard_2_A7.JPG/220px-Leopard_2_A7.JPG",
+    "price": 200000
+  },
+  {
+    "name": "Tiger Tank",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Leopard_2_A7.JPG/220px-Leopard_2_A7.JPG",
+    "price": 200000
+  },
+  {
+    "name": "Tiger Tank",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Leopard_2_A7.JPG/220px-Leopard_2_A7.JPG",
+    "price": 50000
+  }
+]
+
+export default catalog;
